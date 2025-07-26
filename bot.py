45abcad284ebd76e5198c1d3dd4b305d7eb416d7
KirillKozlov <kirillandreevickozlov@yandex.ru>
2025-07-26 20:41:07 +0300

    done, time_exec = 0.8s

--- bot.py
+++ bot.py
@@ -1,18 +1,16 @@
 import os
-import shutil
-import json
+import asyncio
+import logging
+import time
+from dotenv import load_dotenv
 from itertools import islice
+from aiogram import F
 from aiogram import Bot, Dispatcher, types
 from aiogram.types import Message
 from aiogram.filters import CommandStart
 
-from aiogram import F
+from parse_module import main as parse_main
 
-import logging
-
-import time
-from parse_module import main as parse_main 
-from dotenv import load_dotenv
 
 async def main():
     bot = Bot(token=BOT_TOKEN)
@@ -20,20 +18,25 @@ async def main():
 
     @dp.message(CommandStart())
     async def start(message: Message):
-        await message.answer("Привет! Отправь мне поисковый запрос и я верну топ-5 товаров с Wildberries по органическим позициям.")
+        await message.answer(
+            "Привет! Отправь мне поисковый запрос и я верну топ-5 товаров с Wildberries по органическим позициям."
+        )
 
     @dp.message(F.text)
     async def handle_query(message: types.Message):
         query = message.text.strip()
-        print("receive message:",query)
-        
-        await message.answer(f"🔍 Получена ключевая фраза: <b>{query}</b>\nПожалуйста, подождите, идёт обработка...\n(примерное время обработки ~ 40 секунд)", parse_mode="HTML")
-        
+        print("receive message:", query)
+
+        await message.answer(
+            f"🔍 Получена ключевая фраза: <b>{query}</b>\nПожалуйста, подождите, идёт обработка...\n",
+            parse_mode="HTML",
+        )
+
         try:
             start = time.time()
             result = await parse_main(query)
             exec_time = time.time() - start
-            print("exec_time parse_main = ",exec_time)
+            print("exec_time parse_main = ", exec_time)
             first_five = dict(islice(result.items(), 5))
 
             reply = "\u2705 Топ-5 товаров по запросу:\n"
@@ -41,21 +44,24 @@ async def main():
                 url = data["link"]
                 text = data["name"]
                 resp = f'<a href="{url}">{text}</a>'
-                reply += (f"\n{resp}\n"
-                        f"{data['price']}₽ (СПП = 30%)\n"
-                        f"{data['nmReviewRating']} ({data['nmFeedbacks']} отзывов) \n"
-                        f"Органическая позиция: {data['organic_position']}\n"
-                        f"Страница в поиске: {data['page']}\n"
+                reply += (
+                    f"\n{resp}\n"
+                    f"{data['price']}₽ (СПП = 30%)\n"
+                    f"{data['nmReviewRating']} ({data['nmFeedbacks']} отзывов) \n"
+                    f"Органическая позиция: {data['organic_position']}\n"
+                    f"Промо позиция: {data['promo_position']}\n"
+                    f"Страница в поиске: {data['page']}\n"
                 )
-            reply += f"\n время выполнения {exec_time}\n"
+            reply += f"\nВремя обработки: {exec_time:.2f} сек\n"
             await message.answer(reply, parse_mode="HTML")
-        except Exception as e:
-            await message.answer("\u274C Произошла ошибка при обработке запроса.")
+        except Exception:
+            await message.answer("\u274c Произошла ошибка при обработке запроса.")
             logging.exception("Ошибка при обработке запроса")
+
     await dp.start_polling(bot)
+
 
 if __name__ == "__main__":
     load_dotenv()
     BOT_TOKEN = os.getenv("TG_BOT_TOKEN")
-    import asyncio
     asyncio.run(main())
--- parse_module.py
+++ parse_module.py
@@ -1,22 +1,20 @@
-import aiohttp 
+import aiohttp
 import asyncio
 
 from service.parse_wb_site_class import ParseWbSiteClass
 
 
-def sort_key(item):
-    organic_pos = item[1]["organic_position"]
-    # Если organic_position None, возвращаем очень большое число, чтобы элемент попал в конец
-    return (organic_pos is None, organic_pos if organic_pos is not None else 0)
-
 async def main(keyword: str):
-    articles = {}
-    SEMAPHORE = asyncio.Semaphore(2000)
+    SEMAPHORE = asyncio.Semaphore(100)
     parser = ParseWbSiteClass(SEMAPHORE)
+    articles = {}
+    tasks = []
     async with aiohttp.ClientSession() as session:
-        articles = await parser.parse_first_page(session, keyword, articles)
-        for page_number in ['2', '3', '4']:
-            articles = await parser.parse_page_number_(session, keyword, page_number, articles)
+        tasks.append(parser.parse_first_page(session, keyword, articles))
+        tasks.append(parser.parse_page_number_(session, keyword, "2", articles))
+        tasks.append(parser.parse_page_number_(session, keyword, "3", articles))
+        await asyncio.gather(*tasks)
 
-    return dict(sorted(articles.items(), key=sort_key)) # сортировка по возврастанию organic_position
-    
+        return dict(
+            sorted(articles.items(), key=parser.sort_key)
+        )  # сортировка по возврастанию organic_position
